9d8793fde13083aec2efc05e27e0cce4acb36ea8
Andrea <abgomez.ec@gmail.com>
2023-06-23 14:52:19 -0400

    authentication

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,83 @@
 var express = require('express');
 const db = require('../database');
 var router = express.Router();
+const request = require ('request');
 const IP = require ('ip');
-const address = IP.address();
-const ipString = address.toString();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-/* GET home page. */
+//Pagina de inicio 
 router.get('/', function(req, res, next) {
-  let name = 'Andrea Gómez'
+  let name ='Andrea Gómez'
   res.render('index', {
     title: 'Formulario de contacto',
-    name: name,
-  });
-});
- 
-router.post('/', function(req, res, next) {
+    name: name, });});
+
+
+//Ejecuta acciones del formulario
+router.post('/', function(req, res, next) {  
+  //Declaracion de variables locales
+  const captcha = req.body['g-recaptcha-response'];
+  const secretKey = process.env.SecretKey;
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}`;
   let name = req.body.name;
   let email = req.body.email;
   let cell = req.body.cell;
   let comment = req.body.comment;
   let date = new Date();
-  let Datetime = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-  let ip = ipString;
+  let fecha = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  let Datetime = fecha;
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const myIP = ip.split(",")[0];
 
-  db.insert(name, email, cell, comment, Datetime, ip);
 
-  console.log({name, email, cell, comment, Datetime, ip});
-
-  res.redirect('/');
+  //Localizar pais de origen de la IP
+  request(`http://ip-api.com/json/${myIP}`, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      let country = data.country;
+      //Mostrar datos ingresados pos consola
+      console.log({name, email, cell, comment, Datetime, myIP, country});
+      //Insertar daton en la base de datos
+      db.insert(name, email, cell, comment, Datetime, myIP, country);
+      
+      
+      //Enviar email con los datos ingresados 
+      const transporter = nodemailer.createTransport({
+        host: process.env.hostemail,
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.useremail,
+            pass: process.env.passemail
+        }
+      });
+      const mailOptions = {
+        from: process.env.useremail,
+        //Lista de correos 
+        to: ['andreahnzz@gmail.com', 'abgomez.ec@gmail.com', 'programacion2ais@dispostable.com'],
+        subject: 'Task 3: Third Party Connection ',
+        text: 'Un nuevo usuario se ha registrado en el formulario:\n' + 'Nombre: ' + name + '\nCorreo: ' + email + '\nTelefono: ' + cell + '\nMensaje: ' + comment + '\nFecha y hora: ' + Datetime + '\nIP: ' + myIP + '\nUbicacion: ' + country
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Correo electrónico enviado: ' + info.response);
+        }});}});
+        //Validacion de reCAPTCHA 
+        request(url, (err, response, body) => {
+          if (body.success && body.score) {
+            console.log('exitoso')
+          } else {
+            console.log('fracaso')
+          }
+        });
+    //Redirigir a pagina nuevamente
+    res.redirect('/');
 });
 
+//Insertar datos
 router.get('/contactos', function(req, res, next) {
   db.select(function (rows) {
     console.log(rows);
